63491371adfcf0e0a667f06824e64bcc99c9fc2a
nathanlrf <nathanlrf@gmail.com>
2013-11-02 00:03:05 +0800

    GREEDY GIFT GIVERS

---
+++ gift1/gift1.cpp
@@ -0,0 +1,19 @@
+/*
+ID: nathanlrf        
+PROG: gift1
+LANG: C++
+*/
+
+#include<iostream>
+#include<fstream>
+#include<string>
+
+int main()
+{
+	ifstream fin ("gift1.in");
+	ofstream fout ("gift1.out");
+	
+
+
+	return 0
+}

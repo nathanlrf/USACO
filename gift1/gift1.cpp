14b01ed60bd55d57323a989bcfd25871376c16d0
nathanlrf <nathanlrf@gmail.com>
2013-11-02 11:23:30 +0800

    read group member

--- gift1/gift1.cpp
+++ gift1/gift1.cpp
@@ -7,17 +7,57 @@ LANG: C++
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<vector>
+using namespace std;
 
 int main()
 {
-	ifstream fin ("gift1.in");
-	ofstream fout ("gift1.out");
-	
+	//this is for test
+	ofstream test("gift1.in");
+	test<<"5"<<endl;
+	test<<"dave"<<endl;
+	test<<"laura"<<endl;
+	test<<"laura"<<endl;
+	test<<"owen"<<endl;
+	test<<"vick"<<endl;
+	test<<"amr"<<endl;
+	test<<"dave"<<endl;
+	test<<"200 3"<<endl;
+	test<<"laura"<<endl;
+	test<<"owen"<<endl;
+	test<<"vick"<<endl;
+	test<<"owen"<<endl;
+	test<<"500 1"<<endl;
+	test<<"dave"<<endl;
+	test<<"amr"<<endl;
+	test<<"150 2"<<endl;
+	test<<"vick"<<endl;
+	test<<"owen"<<endl;
+	test<<"laura"<<endl;
+	test<<"0 2"<<endl;
+	test<<"amr"<<endl;
+	test<<"vick"<<endl;
+	test<<"vick"<<endl;
+	test<<"0 0"<<endl;
+	//comment section above when submit
+	ifstream fin("gift1.in");
+	ofstream fout("gift1.out");
 	const int size=20;
 	int group_num;
-	int group_mem[size];
+	string group_mem[size];
+	int i;
+	fin>>group_num;
+	for(i=0;i<group_num;i++)
+	{
+		fin>>group_mem[i];
+	}
+
+	fout<<group_num<<endl;
 	
+	for(i=0;i<group_num;i++)
+	{
+		fout<<group_mem[i]<<endl;
+	}
 
-
-	return 0
+	return 0;
 }

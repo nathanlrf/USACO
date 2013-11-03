75b5055654923f39119bdb4f622be92b02f2caa0
nathanlrf <nathanlrf@gmail.com>
2013-11-03 21:19:14 +0800

    calculate how many times 13th fall on each weekday

---
+++ friday/friday.cpp
@@ -0,0 +1,29 @@
+/*
+ID: nathanlrf        
+PROG: gift1
+LANG: C++
+*/
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+int main()
+{ 
+	//this is for test
+	ofstream test("friday.in");
+	test<<"1"<<endl;//try calculate 13th in 1900
+	
+	ifstream fin("friday.in");
+	ofstream fout("friday.out");
+	
+	const int size=7;
+	int week[size];//week[0]=Monday,week[6]=Sunday
+	
+
+
+	return 0;
+
+}
+

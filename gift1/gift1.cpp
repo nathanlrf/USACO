e0d2582b22ea19f9e787340f971b7a78676c6e51
nathanlrf <nathanlrf@gmail.com>
2013-11-03 10:33:43 +0800

    add function index to find index of array according to name

--- gift1/gift1.cpp
+++ gift1/gift1.cpp
@@ -4,6 +4,10 @@ PROG: gift1
 LANG: C++
 */
 
+/*
+relate name with their money:name are stored in group_mem[i],use index i to find an element in money[i]
+print the same i in group_mem and money thus they are binded
+*/
 #include<iostream>
 #include<fstream>
 #include<string>
@@ -13,6 +17,7 @@ LANG: C++
 using namespace std;
 
 int str2int(string rec_num);
+int index(string name,string* arr_ptr,int NP);
 
 int main()
 {
@@ -63,7 +68,7 @@ int main()
         {
                 fout<<group_mem[i]<<endl;
         }
-        
+		
 		string money,rec_num;
 		int j;
 		string giver;
@@ -71,25 +76,27 @@ int main()
 		int money_int,rec_num_int;
 		
 		
-		while(fin)
-		{
+	//	while(fin)
+	//	{
 			fin>>giver;
+		
 			fin>>money;//money to give
 			money_int=str2int(money);
 			fin>>rec_num;//number of receivers
 			rec_num_int=str2int(rec_num);
 			
-			for(j=0;j<rec_num_int;j++)
-			{
-				fin>>rec;//store receivers' name in rec
-			}
+	//		for(j=0;j<rec_num_int;j++)
+	//		{
+	//			fin>>rec;//store receivers' name in rec
+	//		}
 			fout<<"giver is "<<giver<<endl;
+			fout<<giver<<"'s index is "<<index(giver,group_mem,NP)<<endl;
 			//fout<<money<<endl;
 			fout<<money_int<<endl;
 			//fout<<rec_num<<endl;
 			fout<<rec_num_int<<endl;
 		
-		}
+	//	}
 		return 0;
 		
 
@@ -101,4 +108,15 @@ int str2int(string str)
 	ss<<str;
 	ss>>integer;
 	return integer;
+}
+int index(string name,string* arr_ptr,int NP)//find index of a given name, error return 111
+{
+	int i;
+	for(i=0;i<NP;i++)
+	{
+		if(arr_ptr[i]==name)
+			return i;
+		else
+			return 111;
+	}
 }

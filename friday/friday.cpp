8e954945113740fb1b5579709f11bc9854f7a0f7
nathanlrf <nathanlrf@gmail.com>
2013-11-05 12:46:32 +0800

    wrong on 256

--- friday/friday.cpp
+++ friday/friday.cpp
@@ -1,6 +1,6 @@
 /*
 ID: nathanlrf        
-PROG: gift1
+PROG: friday
 LANG: C++
 */
 #include<iostream>
@@ -15,22 +15,22 @@ int main()
 { 
 	//this is for test
 	ofstream test("friday.in");
-	test<<"100"<<endl;//try calculate 13th in 1900
+	test<<"256"<<endl;//try calculate 13th in 1900
 	
 	ifstream fin("friday.in");
 	ofstream fout("friday.out");
 	
 	const int week_size=7;
 	const int month_size=12;
-	const start_year=1900;
+	const int start_year=1900;
 	int end_year;
 	int week[week_size];//week[0]=Monday,week[6]=Sunday
-	int _31_day[7];
-	int _30_day[4];
 	int month[month_size];
-	int Feb;//depends on leap(N)
-	int N;
-	int i;
+
+	int Feb;//depend on leap year
+	int N;//how many years
+	int i,j;//array index
+	int temp;//calculate the cycle of weeks
 
 	fin>>N;
 	Feb=leap(N);
@@ -38,23 +38,70 @@ int main()
 	//initialize week[]
 	for(i=0;i<week_size;i++)
 	{
-		week[i]=0;
+		if(i==5)
+			week[i]=1;
+		else	
+			week[i]=0;
 	}
-	for(i=0;i<month_size;i++)
+	
+	//big loop for N year
+	int start=5;//1900 jan 13th is on Saturday 
+	for(j=0;j<N;j++)
 	{
-		if(3==i||5==i||8==i||10==i)
-			month[i]=30;
-		else if(i==1)
-			month[i]=leap(N);
-		else
-			month[i]=31;
-	}
+		if(j>0)
+		{
+			start=temp+month[i]%7;//i=11; get jan 13th in the next year
+			if(start<7)
+				start=start;
+			else
+				start%=7;
+			++week[start];
+		}
 
+		for(i=0;i<month_size;i++)
+		{	
+			if(3==i||5==i||8==i||10==i)
+				month[i]=30;
+			else if(i==1)
+				month[i]=leap(j+1900);
+			else
+				month[i]=31;
+		}
+		//small loop for 1 year
+		//find 13th in each month in 1901
+		//1st month is on Sat temp=5;then 31%7=3,temp=5+3=8;then 28%7=0,temp=1;then 31%7=3,temp=1+3=4;then 30%7=2,temp=4+2=6
+		for(i=0,temp=start;i<month_size-1;i++)
+		{	
+			temp+=month[i]%7;
+			if(temp<7)
+				temp=temp;
+			else
+				temp%=7;
+			++week[temp];
+		}
+		
+
+	}
+	
 
 //output
-	fout<<"from 1900 to "<<end_year<<endl;
+/*	fout<<"from 1900 to "<<end_year<<endl;
 	fout<<"there is "<<leap(N)<<"days in feb of "<<end_year<<endl;
+	for(i=0;i<week_size;i++)
+	{
+		fout<<week[i]<<' ';
+	}
+	fout<<endl;
+*/
 
+	int Sat_to_Fri[week_size];
+	for(i=0;i<week_size-1;i++)
+	{
+		Sat_to_Fri[i]=week[(i+5)%7];
+		fout<<Sat_to_Fri[i]<<' ';
+	}
+	fout<<week[4]<<endl;
+	
 	return 0;
 
 }
